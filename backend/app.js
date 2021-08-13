d1ec1b59113e333018ec22a688f07e56ed84b3af
ilzira <murtazina.ilzi@yandex.ru>
2021-08-13 14:06:58 +0300

    logger and crashtest

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const NotFoundError = require('./errors/NotFoundError');
 const { errorHandler } = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
@@ -48,9 +49,18 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(authRouter);
 app.use(userRouter);
 app.use(cardRouter);
+
+app.use(requestLogger);
+app.use(errorLogger);
 
 app.get('*', (req) => { throw new NotFoundError(`По адресу ${req.path} ничего не найдено`); });
 
